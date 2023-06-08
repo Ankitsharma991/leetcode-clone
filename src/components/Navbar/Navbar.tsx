273f84c8373ecd69840e1d78e37b9ae76ae3b087
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-08 17:57:43 +0530

    AuthPage Created

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between sm:px-12 md:px-24">
+      <Link href="/" className="h-20">
+        <img src="/logo.png" alt="LeetClone" className="h-full" />
+      </Link>
+      <div className="flex items-center">
+        <button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md  text-sm font-medium hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent transition duration-300 ease-in-out">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -8,6 +8,9 @@ const AuthPage: React.FC<AuthPageProps> = () => {
     <div className="bg-gradient-to-b from-gray-600 to-black  h-screen relative">
       <div className="max-w-7xl mx-auto">
         <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <img src="/hero.png" alt="hero_image" />
+        </div>
       </div>
     </div>
   );
