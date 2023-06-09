db35377717edba8d4aa2f8fada9c2193fd26935b
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-09 19:00:51 +0530

    Integrated Firebase

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,16 +1,49 @@
 import { authModalState } from "@/atoms/authModalAtom";
-import React from "react";
+import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
+import { auth } from "@/firebase/firebase";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
 
 type SignupProps = {};
 
 const Signup: React.FC<SignupProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
-  const handleClick = (type: "login" | "register" | "forgotPassword") => {
-    setAuthModalState((prev) => ({ ...prev, type }));
+  const handleClick = () => {
+    setAuthModalState((prev) => ({ ...prev, type: "login" }));
   };
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    displayName: "",
+    password: "",
+  });
+
+  const router = useRouter();
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const newUser = await createUserWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      if (!newUser) return;
+      router.push("/");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <form className="space-y-6 px-6 pb-4">
+    <form className="space-y-6 px-6 pb-4" onSubmit={handleRegister}>
       <h3 className="text-xl font-medium text-white">Register to LeetClone</h3>
       <div>
         <label
@@ -20,6 +53,7 @@ const Signup: React.FC<SignupProps> = () => {
           Email
         </label>
         <input
+          onChange={handleChangeInput}
           type="email"
           name="email"
           id="email"
@@ -35,6 +69,7 @@ const Signup: React.FC<SignupProps> = () => {
           Name
         </label>
         <input
+          onChange={handleChangeInput}
           type="displayName"
           name="displayName"
           id="displayName"
@@ -50,6 +85,7 @@ const Signup: React.FC<SignupProps> = () => {
           Password
         </label>
         <input
+          onChange={handleChangeInput}
           type="password"
           name="password"
           id="password"
---
+++ src/firebase/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_APP_ID,
+//   measurementId: "G-B1VG6C85SM",
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQNiWvKBW67hfnh9I1dZgqxdNImBMsNUA",
+  authDomain: "leetclone-55793.firebaseapp.com",
+  projectId: "leetclone-55793",
+  storageBucket: "leetclone-55793.appspot.com",
+  messagingSenderId: "241845417682",
+  appId: "1:241845417682:web:d1f41d97a0714799d81f0b",
+  measurementId: "G-B1VG6C85SM",
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth, firestore, app };
